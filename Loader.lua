19028bebbf9a27d850b774bb702f439036503da4
66-75-63-6B-79-6F-75 <109988692+66-75-63-6B-79-6F-75@users.noreply.github.com>
2022-07-30 21:56:30 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,11 @@
+--[[
+CONGRATULATIONS IT APPEARS YOUVE FOUND THE ONLY UNOBFUSCATED CODE IN THE HUB!!!!! (other than the ui lib)
+]]
+
 local function AddGame(Url, GameId)
   if game.PlaceId == GameId then
     loadstring(game:HttpGet(tostring(Url), true))()
   end
 end
 
+AddGame("https://pst.klgrth.io/paste/pypu3/raw", 3823781113)
